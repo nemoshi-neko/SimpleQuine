2ce5d42e0da5bd444f2a72e293b0482363151ebb
nemoshi-neko <188776090+nemoshi-neko@users.noreply.github.com>
2025-10-16 15:46:14 +0900

    Implement a simple quine in C and add VSCode settings for file associations

--- quine01.c
+++ quine01.c
@@ -1,1 +1,8 @@
-char *s = "char *s = %c%s%c; main() { printf(s, 34, s, 34); }";
+#include <stdio.h>
+
+char* s = "#include <stdio.h>%c%cchar* s = %c%s%c;%c%cint main() {%c%c%c%cprintf(s, 10, 10, 34, s, 34, 10, 10, 10, 10, 10);%c%c%c%creturn 0;%c}%c";
+
+int main() {
+    printf(s, 10, 10, 34, s, 34, 10, 10, 10, 10, 10);
+    return 0;
+}
